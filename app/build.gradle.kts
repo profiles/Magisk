fd72696af088c2eebf082b5c25bb2784eb7ffe30
anonymous <anonymous@anonymous.com>
2025-01-12 00:27:32 +0800

    update androidx.room to fix support AppleSilicon, https://developer.android.com/jetpack/androidx/releases/room#2.4.0-alpha03

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -111,7 +111,7 @@ dependencies {
     implementation("com.squareup.moshi:moshi:${vMoshi}")
     kapt("com.squareup.moshi:moshi-kotlin-codegen:${vMoshi}")
 
-    val vRoom = "2.3.0"
+    val vRoom = "2.4.0-alpha03"
     implementation("androidx.room:room-runtime:${vRoom}")
     implementation("androidx.room:room-ktx:${vRoom}")
     kapt("androidx.room:room-compiler:${vRoom}")
